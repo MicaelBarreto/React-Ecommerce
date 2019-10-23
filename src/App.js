550f78ba370641c34e3110aea8ad967acfbe8893
micaelbarreto <micaelplayer98@gmail.com>
2019-10-22 23:11:32 -0300

    Shop page and starting sign in/sign up page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
 import './App.css';
+
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
+import Sign from './pages/sign/sign';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/sign' component={Sign} />
       </Switch>
     </div>
   );
